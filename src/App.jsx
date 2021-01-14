36458f52dd79db8f655756913a1751289cec98ec
Shevchuk-Vadym <lorelliand@gmail.com>
2021-01-14 22:21:45 +0200

    add mealMenu

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { MainMeal } from "./MainMeal";
+import styles from "./App.module.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <MainMeal />
+      </div>
+    );
+  }
+}
+
+export { App };
