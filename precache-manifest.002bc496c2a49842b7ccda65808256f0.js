8531f98ff7948033125eee60d6237f186eaf6a15
Shevchuk-Vadym <lorelliand@gmail.com>
2021-01-14 23:55:57 +0200

    Updates

---
+++ precache-manifest.002bc496c2a49842b7ccda65808256f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9965d1a451ee4bf9be78953136e26a",
+    "url": "/MealMenu/index.html"
+  },
+  {
+    "revision": "da545336c4792cc9ea73",
+    "url": "/MealMenu/static/css/main.930fc0a2.chunk.css"
+  },
+  {
+    "revision": "cd5b9be68622b49f555c",
+    "url": "/MealMenu/static/js/2.ebc36a17.chunk.js"
+  },
+  {
+    "revision": "da545336c4792cc9ea73",
+    "url": "/MealMenu/static/js/main.73aedbe2.chunk.js"
+  },
+  {
+    "revision": "b732bd9906e97cd5935c",
+    "url": "/MealMenu/static/js/runtime-main.0ab3d9fb.js"
+  }
+]);
